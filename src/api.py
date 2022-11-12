bf870e8cc0fd3a7a2d7637d08f287dda1cb75b50
Charles Ginane <charles.ginane@gmail.com>
2022-11-12 17:37:39 +0100

    Add db connection to API

--- src/api.py
+++ src/api.py
@@ -1,12 +1,24 @@
 from aiohttp import web
+import psycopg2
+from psycopg2 import Error
+
+from db import DataBase
+
+DB_USER = "chgigi"
+DB_PASS = "dbaccess12345"
+DB_NAME = "homeserver"
+
 
 class App:
-    def __init__(self, address="127.0.0.1", port=3333):
+    def __init__(self, address="127.0.0.1", port=3333, db_address="127.0.0.1", db_port=5432):
         self._address = address
         self._port = 3333
         self._app = web.Application()
         self._handler = Handler()
         self._app.add_routes([web.get('/ping', self._handler._get_ping)])
+        self._db = DataBase(DB_USER, DB_PASS, db_address, db_port, DB_NAME)
+        # Try a connection
+        self._db.connect()
 
     def run(self):
         web.run_app(self._app, host=self._address, port=self._port)
@@ -18,7 +30,5 @@ class Handler:
     async def _get_ping(self, request):
         return web.Response(text="Pong")
 
-
 app = App()
 app.run()
-
