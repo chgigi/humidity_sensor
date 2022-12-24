b41a47573cc89eb3dff9dda9a98551cd92431dce
Charles Ginane <charles.ginane@gmail.com>
2022-12-24 13:10:01 +0100

    Change db access

--- src/api.py
+++ src/api.py
@@ -7,18 +7,14 @@ import json
 from db import DataBase
 from dht import get_temperature_from_dht22_local
 
-DB_USER = "chgigi"
-DB_PASS = "dbaccess12345"
-DB_NAME = "homeserver"
-
 
 class App(web.Application):
-    def __init__(self, logger, address="0.0.0.0", port=3333, db_address="127.0.0.1", db_port=5432):
+    def __init__(self, logger, address="0.0.0.0", port=3333, db_address="127.0.0.1", db_port=5432, db_user=None, db_pass=None, db_name=None):
         self._logger = logger
         self._address = address
         self._port = port
         self._app = web.Application()
-        self._db = DataBase(DB_USER, DB_PASS, db_address, db_port, DB_NAME, self._logger)
+        self._db = DataBase(db_user, db_pass, db_address, db_port, db_name, self._logger)
 
         self._logger.info(f"configure api with address: http://{self._address}:{self._port}")
         self._logger.info(f"connect to DataBase at address: {db_address}:{db_port}")
--- src/run.py
+++ src/run.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 import logging
 import os
 import sys
@@ -15,6 +16,7 @@ def run():
     parser.add_argument('--db-address', type=str, help='Address of the DataBase', default="127.0.0.1")
     parser.add_argument('--db-port', type=int, help='Port of the DataBase', default=5432)
     parser.add_argument('--logfile', type=str, help='log file to store logs', default="logs/humiditySensor.log")
+    parser.add_argument('--db-access', type=str, help='File access', required=True)
 
     args = parser.parse_args()
 
@@ -30,6 +32,20 @@ def run():
     handler.setFormatter(formatter)
     logger.addHandler(handler)
 
-    app = App(logger=logger, address="0.0.0.0", port=args.port, db_address=args.db_address, db_port=args.db_port)
+    # Get db access credentials
+    db_user = None
+    db_pass = None
+    db_name = None
+    try:
+        with open(args.db_access) as f:
+            json_file = json.load(f)
+            db_user = json_file['user']
+            db_pass = json_file['password']
+            db_name = json_file['name']
+    except Exception as e:
+        logger.error("Can't read the logging access file for database", e)
+        sys.exit(-1)
+    app = App(logger=logger, address="0.0.0.0", port=args.port, db_address=args.db_address,
+              db_port=args.db_port, db_user=db_user, db_pass=db_pass, db_name=db_name)
     app.run()
     
